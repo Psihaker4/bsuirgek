8c7b244189cf539aded03cd64097d022936bcba4
ilyashmayst <ilyashmaust1999@gmail.com>
2018-03-08 12:32:20 +0300

    new view button and new backend

--- client/src/main/java/com/agt/bsuirgek/client/Controller/MainMenuController.java
+++ client/src/main/java/com/agt/bsuirgek/client/Controller/MainMenuController.java
@@ -1,9 +1,12 @@
 package com.agt.bsuirgek.client.Controller;
 
 import com.agt.bsuirgek.client.network.ProcesingRequests;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -21,6 +24,16 @@ public class MainMenuController {
 
     @FXML
     public TextField tfFileName;
+
+    @FXML
+    public ListView<String> lvSetTemplate;
+
+    private ObservableList<String> list = FXCollections.observableArrayList("аблон 1", "шаблон 2", "шаблон 3", "аблон 1", "шаблон 2", "шаблон 3", "аблон 1", "шаблон 2", "шаблон 3", "аблон 1", "шаблон 2", "шаблон 3");
+
+    @FXML
+    public void initialize() {
+        lvSetTemplate.setItems(list);
+    }
 
     public void btnLoadAction(ActionEvent actionEvent) {
         file = new VBoxForLoadFileController().actionBtnLoad(tfFileName, btnOK);
