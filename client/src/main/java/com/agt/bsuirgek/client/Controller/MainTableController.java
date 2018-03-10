e608c42324dc3e68b79f8c58ee81ebc494db70bd
ilyashmayst <ilyashmaust1999@gmail.com>
2018-03-10 23:42:48 +0300

    new view button and new backend

--- client/src/main/java/com/agt/bsuirgek/client/Controller/MainTableController.java
+++ client/src/main/java/com/agt/bsuirgek/client/Controller/MainTableController.java
@@ -3,31 +3,47 @@ package com.agt.bsuirgek.client.Controller;
 import com.agt.bsuirgek.client.Object.Student;
 import com.agt.bsuirgek.client.Object.TempMemory;
 import com.agt.bsuirgek.client.UI.MainTable;
-import com.sun.javafx.scene.control.skin.TableColumnHeader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainTableController {
 
     private MainTable parent;
-    private ArrayList<TableColumn<Student, String>> mapColumn = new ArrayList<>();
+    private Map<String, TableColumn<Student, String>> mapColumn = new HashMap<>();
+    private int quantityColumn = 0;
 
 
     @FXML
     public TableView<Student> mainTable;
 
-    public MainTableController(MainTable parent){
+    public MainTableController(MainTable parent) {
         this.parent = parent;
+    }
 
-        for(Map.Entry<String, String> entry : TempMemory.listTempStudent.get(1).getMapStident().entrySet()){
-            mapColumn.add( new TableColumn<Student, String>(entry.getKey()));
-            System.out.println(mapColumn.get(mapColumn.size() - 1));
-            mainTable.getColumns().add(mapColumn.get(mapColumn.size()-1));
+    @FXML
+    public void initialize() {
+
+        ObservableList<Student> items = FXCollections.observableArrayList(TempMemory.listTempStudent);
+        mainTable.setItems(items);
+
+        for (Map.Entry<String, String> entry : TempMemory.listTempStudent.get(1).getMapStident().entrySet()) {
+            quantityColumn++;
+            mapColumn.put(entry.getKey() ,new TableColumn<Student, String>(TempMemory.RUS_NAME_FIELD_STUDENT.get(entry.getKey())));
+            mapColumn.get(entry.getKey()).setCellValueFactory(new PropertyValueFactory<>(entry.getKey()));
+            mainTable.getColumns().add(mapColumn.get(entry.getKey()));
+        }
+        double widthColumn = mainTable.getPrefWidth()/quantityColumn;
+        for(Map.Entry<String, TableColumn<Student, String>> entry : mapColumn.entrySet()){
+            entry.getValue().setPrefWidth(widthColumn);
+            entry.getValue().setMinWidth(widthColumn);
+            entry.getValue().setMaxWidth(widthColumn);
         }
     }
 }
